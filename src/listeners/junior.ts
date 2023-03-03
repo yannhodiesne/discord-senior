cb8b372e7629cd3ef6f9cbdd1502a4fc41d34a82
Yann Hodiesne <yann.hodiesne@viacesi.fr>
2023-03-03 16:56:04 +0100

    Less talkative to junior

--- src/listeners/junior.ts
+++ src/listeners/junior.ts
@@ -10,7 +10,7 @@ export class JuniorListener extends Listener {
 	public async run(message: Message) {
 		if (message.author.id !== '1007028483505012807') return;
 
-		if (Math.random() < 0.2) return;
+		if (Math.random() < 0.8) return;
 
 		await reply(message, 'tg');
 	}
